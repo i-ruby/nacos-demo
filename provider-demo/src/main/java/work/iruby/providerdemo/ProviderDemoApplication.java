1defea7dc58d496ad89b0babed48b2f30e0704b2
iruby <1479867360@qq.com>
2021-07-15 00:05:01 +0800

    init

---
+++ provider-demo/src/main/java/work/iruby/providerdemo/ProviderDemoApplication.java
@@ -0,0 +1,15 @@
+package work.iruby.providerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ProviderDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderDemoApplication.class, args);
+    }
+
+}
